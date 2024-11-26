589249c70dcf69f77f3d4b32510c428c219bc890
Aiden <aiden.p.developer@gmail.com>
2024-11-26 14:30:30 -0400

    ahora con arreglando el tomlv3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: 3000,
+  // server: 3000,
   plugins: [react()],
   build: {
     outDir: "build", // Cambia esto si deseas que la salida se genere en un directorio específico
