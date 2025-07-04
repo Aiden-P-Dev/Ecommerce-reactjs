0815a06c1ceaa5263a1b37e5a3ff75dea1ce1d17
Aiden <aiden.p.developer@gmail.com>
2025-07-03 22:08:18 -0400

    aver con netlify

--- backend/src/config.js
+++ backend/src/config.js
@@ -2,6 +2,8 @@ export const TOKEN_SECRET = "clavesecreta";
 
 export const MONGODB_URI =
   process.env.MONGODB_URI ||
-  "mongodb+srv://aidenpdeveloper:kF8so8iBifTbNecI@cluster.rqppn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster";
+  "postgresql://neondb_owner:npg_h5qRMBaDWzP1@ep-steep-mode-a5k82g1t-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require";
 
 export const PORT = process.env.PORT || 3000;
+
+// "mongodb+srv://aidenpdeveloper:kF8so8iBifTbNecI@cluster.rqppn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster";
