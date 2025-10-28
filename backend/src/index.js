3f8f3d35a142ec7baeae2f3d859dd3e35de84d60
Aiden <aiden.p.developer@gmail.com>
2025-10-28 16:17:53 -0400

    build16

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,11 @@
 import app from "./app.js";
 import { conectDB } from "./db.js";
+import { PORT } from "./config.js";
 
 conectDB();
 
+// desarrollo local
+// app.listen(3000);
+// console.log(`server port: ${PORT}`);
+// desarrollo local
 export default app;
