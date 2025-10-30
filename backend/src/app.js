740860edc00efe126a3d126567c32c4bbd923f0d
Aiden <aiden.p.developer@gmail.com>
2025-10-30 15:27:05 -0400

    build28

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,8 +14,12 @@ app.use(
     credentials: true,
   })
 );
+
 app.use(morgan("dev"));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
