043f4418eb37c1c818dce4a0355396d44bde37b4
Aiden <aiden.p.developer@gmail.com>
2025-11-01 16:21:07 -0400

    build162

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,18 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // ----------------------------------------------------
-  // SOLUCIÓN CLAVE PARA VERDE/VERCEL:
-  // Fuerza a Vite a construir el index.html referenciando
-  // todos los assets desde la raíz (/).
   base: "/",
-  // ----------------------------------------------------
-  build: {
-    // Asegura que la salida de la compilación siempre esté en 'dist'
-    outDir: "dist",
-    // Asegura que Vite maneje rutas relativas correctamente en el build
-    assetsDir: "assets",
+
+  plugins: [react()],
+
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 });
