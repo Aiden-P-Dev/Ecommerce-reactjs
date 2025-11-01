39915ce744b50ad94eefe1c862589f0258f33cb1
Aiden <aiden.p.developer@gmail.com>
2025-11-01 12:11:24 -0400

    build84

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // ...
-  base: "./", // O '/frontend/'
-  build: {
-    outDir: "dist",
-    // ...
-  },
-  // ...
+  // CLAVE: Establece la ruta base a un punto. Esto fuerza a Vite a usar rutas
+  // relativas (ej: src="./assets/...") en el index.html, lo cual es más
+  // compatible con la forma en que Vercel sirve archivos estáticos en monorepos.
+  base: "./",
+  plugins: [react()],
 });
