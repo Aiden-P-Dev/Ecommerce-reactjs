1a173df5d0097f9af6dde9751f574c6957f489f4
Aiden <aiden.p.developer@gmail.com>
2025-11-01 16:39:48 -0400

    build167

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,6 @@
-import express from "express";
-import { connectDB } from "./db.js";
-import { PORT } from "./config.js";
-
-const app = express();
+import app from "./app.js";
+import { connectDB } from "./database.js";
+// import { PORT } from "./config.js"; // No necesitamos PORT para Vercel
 
 connectDB();
 
