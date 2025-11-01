0a0df8ca5da6bfa7a789db7276163c4633e6a07c
Aiden <aiden.p.developer@gmail.com>
2025-11-01 13:12:52 -0400

    build107

--- backend/src/app.js
+++ backend/src/app.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
-app.use("/api", authRoutes);
-app.use("/api", taskRoutes);
+app.use("/auth", authRoutes);
+app.use("/tasks", taskRoutes);
 
 export default app;
