9b8f642a714761fa6538ddf4cfb3cb09034c2357
Aiden <aiden.p.developer@gmail.com>
2025-11-01 11:41:52 -0400

    build75

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // ¡¡¡ESTO ES LO QUE FALTA Y RESUELVE EL PROBLEMA!!!
-  base: "./",
-  plugins: [react()],
+  // ...
+  base: "./", // O '/frontend/'
   build: {
     outDir: "dist",
+    // ...
   },
+  // ...
 });
