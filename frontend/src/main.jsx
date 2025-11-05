0f4481d3f485f0de92e24d48d9827826e7ad7eda
Aiden <aiden.p.developer@gmail.com>
2025-11-05 17:15:23 -0400

    build206

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./Components/Main.css";
-// import "./elproyectodemivida.css";
+import "./elproyectodemivida.css";
 // import "./viewtransition.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
